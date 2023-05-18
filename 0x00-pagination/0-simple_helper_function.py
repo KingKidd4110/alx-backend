bea9ea10601b5acd06a881e997936d4f322746d1
KingKidd4110 <ayiendabrian@gmail.com>
2023-05-18 11:47:12 +0300

    tuple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ function parameters"""
     start_index = (page - 1) * page_size
     end_index = start_idex + page_size
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,7 +6,7 @@ import math
 from typing import List
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ function parameters"""
     start_index = (page - 1) * page_size
     end_index = start_idex + page_size
